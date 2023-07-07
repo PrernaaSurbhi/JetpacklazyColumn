943ad548f940b60830fd5354b1d0934ef249ec98
PrernaSurbhi5 <Prerna.Surbhi@tsb.co.uk>
2023-07-07 20:55:54 +0530

    Creating an ImageCard

--- app/src/main/java/com/example/jetpackcomposerecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposerecyclerview/MainActivity.kt
@@ -1,15 +1,30 @@
 package com.example.jetpackcomposerecyclerview
 
 import android.os.Bundle
+import android.os.ParcelFileDescriptor
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.DataManger
 import com.example.screens.EmployeeListScreen
 import kotlinx.coroutines.CoroutineScope
@@ -27,10 +42,57 @@ class MainActivity : ComponentActivity() {
         }
 
         setContent {
-           App()
+//           App()
+
+            val painter = painterResource(id = R.drawable.snow_man)
+            val description = "snow man is playing"
+            val title = "snow man is playing"
+            
+            Box(modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .padding(16.dp)
+            ) {
+                ImageCard(painter,description,title, modifier = Modifier.fillMaxWidth())
+            }
+
+
+
         }
     }
 }
+
+@Preview(showBackground = true)
+@Composable
+private fun ImageCard(
+    painter: Painter,
+    contentDescription:String,
+    title:String,
+    modifier: Modifier){
+      Card(
+          modifier  = Modifier.fillMaxWidth(),
+          shape = RoundedCornerShape(15.dp),
+
+      ) {
+           Box(modifier = Modifier.height(200.dp)) {
+               Image(painter = painter,
+                   contentDescription = contentDescription,
+               contentScale = ContentScale.Crop)
+           }
+           Box(
+               modifier = Modifier
+                   .fillMaxSize()
+                   .padding(12.dp),
+               contentAlignment = Alignment.BottomStart
+           ) {
+               Text(title,style= TextStyle(color = Color.White, fontSize = 16.sp))
+           }
+      }
+    }
+
+
+
+
+
 
 @Composable
 fun App(){
