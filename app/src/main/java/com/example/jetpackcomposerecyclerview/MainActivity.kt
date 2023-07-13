aa1c438effd0bd54b0b235b9917c73166956d54c
PrernaSurbhi5 <Prerna.Surbhi@tsb.co.uk>
2023-07-13 16:10:16 +0530

    ConstraintsLayout implementation

--- app/src/main/java/com/example/jetpackcomposerecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposerecyclerview/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.ParcelFileDescriptor
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -25,13 +26,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 import com.example.DataManger
 import com.example.screens.EmployeeListScreen
 import kotlinx.coroutines.CoroutineScope
@@ -66,12 +73,48 @@ class MainActivity : ComponentActivity() {
 
          //   ListByLazyColumn()
 
-            ListByLazyColumItemsIndexed()
+          //  ListByLazyColumItemsIndexed()
+
+            ConstraintsLayoutFun()
 
         }
     }
 }
+
 @Preview
+@Composable
+private fun ConstraintsLayoutFun(){
+   val constraintsSet = ConstraintSet{
+       val cyanBox = createRefFor("cyanBox")
+       val redBox = createRefFor("redBox")
+
+       constrain(cyanBox){
+           top.linkTo(parent.top)
+           start.linkTo(parent.start)
+           width = Dimension.value(100.dp)
+           height = Dimension.value(100.dp)
+       }
+
+       constrain(redBox){
+           top.linkTo(parent.top)
+           start.linkTo(cyanBox.end)
+           width = Dimension.value(100.dp)
+           height = Dimension.value(100.dp)
+       }
+   }
+
+   ConstraintLayout(constraintsSet, modifier = Modifier.fillMaxSize()) {
+       Box(modifier = Modifier
+           .background(Color.Cyan)
+           .layoutId("cyanBox")
+       )
+       Box(modifier = Modifier
+           .background(Color.Red)
+           .layoutId("redBox")
+       )
+   }
+}
+
 @Composable
 private fun ListByLazyColumItemsIndexed(){
     // lazy column is scrollable by default
