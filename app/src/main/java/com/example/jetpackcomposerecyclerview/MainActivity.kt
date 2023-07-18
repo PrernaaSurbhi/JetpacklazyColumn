f7498bb890a80d4b380f1e74d1916bb36578a4c8
PrernaSurbhi5 <Prerna.Surbhi@tsb.co.uk>
2023-07-19 01:59:41 +0530

    RecyclerView of PersonDetail implementation by JetpackCompose.

--- app/src/main/java/com/example/jetpackcomposerecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposerecyclerview/MainActivity.kt
@@ -1,18 +1,21 @@
 package com.example.jetpackcomposerecyclerview
 
+import CustomItem
 import android.os.Bundle
-import android.os.ParcelFileDescriptor
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -26,26 +29,26 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.layout
 import androidx.compose.ui.layout.layoutId
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
 import androidx.constraintlayout.compose.Dimension
 import com.example.DataManger
+import com.example.model.Person
+import com.example.repository.PersonRepository
 import com.example.screens.EmployeeListScreen
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
+@ExperimentalFoundationApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -75,11 +78,50 @@ class MainActivity : ComponentActivity() {
 
           //  ListByLazyColumItemsIndexed()
 
-            ConstraintsLayoutFun()
+          //  ConstraintsLayoutFun()
 
+          //  ListItemdisplay()
+
+            PersonListView()
         }
     }
 }
+
+@Composable
+private fun PersonListView(){
+    val repository = PersonRepository()
+    val getAlldataItems: List<Person> =repository.getAllData()
+
+    LazyColumn(
+        contentPadding = PaddingValues(12.dp)
+    ) {
+        items(items = getAlldataItems) { person ->
+            CustomItem(person = person)
+        }
+    }
+}
+
+@Composable
+private fun ListItemdisplay(){
+    val rememberScrollState = rememberScrollState()
+    Column(
+        modifier = Modifier.verticalScroll(rememberScrollState)
+    ) {
+        for(i in 1..50){
+            Text(
+                text ="Item in $i",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 24.dp)
+            )
+        }
+
+    }
+}
+
 
 @Preview
 @Composable
