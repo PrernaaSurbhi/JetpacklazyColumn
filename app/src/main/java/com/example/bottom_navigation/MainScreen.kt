7bf3abd194d1edc26263b1db6fbe6e634381c0c5
PrernaSurbhi5 <Prerna.Surbhi@tsb.co.uk>
2023-07-22 15:00:28 +0530

    Added Jetpack logic for Bottom navigation

--- app/src/main/java/com/example/bottom_navigation/MainScreen.kt
+++ app/src/main/java/com/example/bottom_navigation/MainScreen.kt
@@ -14,7 +14,7 @@ import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-
+import com.example.Screens
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -22,9 +22,61 @@ import androidx.navigation.compose.rememberNavController
 fun MainScreen(){
     val navController = rememberNavController()
     Scaffold(
-        bottomBar = {  }
+        bottomBar = { BottomBar(navController) }
     ) {
         BottomNavGraph(navController = navController)
     }
+}
 
+@Composable
+fun BottomBar(navController: NavHostController){
+    val screens = listOf(
+        Bottom_bar_screens.Home,
+        Bottom_bar_screens.Profile,
+        Bottom_bar_screens.Settings
+    )
+
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+
+    BottomNavigation {
+        screens.forEach { screen ->
+            if (currentDestination != null) {
+                AddItem(
+                    screens = screen,
+                    currentDestination = currentDestination,
+                    navController = navController
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun RowScope.AddItem(
+    screens:Bottom_bar_screens,
+    currentDestination: NavDestination,
+    navController: NavHostController
+){
+    BottomNavigationItem(
+        label = {
+            Text(text = screens.title)
+        },
+        icon = {
+            Icon(
+                imageVector = screens.icon,
+                contentDescription = "Navigation Icon"
+            )
+        },
+        selected = currentDestination?.hierarchy?.any {
+            it.route == screens.route
+        } == true,
+        unselectedContentColor = LocalContentColor.current.copy(alpha = ContentAlpha.disabled),
+        onClick = {
+            navController.navigate(screens.route) {
+                popUpTo(navController.graph.findStartDestination().id)
+                launchSingleTop = true
+            }
+        }
+    )
 }
