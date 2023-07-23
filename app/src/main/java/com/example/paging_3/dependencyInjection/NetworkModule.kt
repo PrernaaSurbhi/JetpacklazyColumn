9f04bdf18267bfcb6d55b0b0b097f96bcadfae1c
PrernaSurbhi5 <Prerna.Surbhi@tsb.co.uk>
2023-07-23 14:40:57 +0530

    Added NetworkModule Module class for getting UnspalshApi class. Updated the Constant file class with BASE_URL

--- app/src/main/java/com/example/paging_3/dependencyInjection/NetworkModule.kt
+++ app/src/main/java/com/example/paging_3/dependencyInjection/NetworkModule.kt
@@ -1,5 +1,47 @@
 package com.example.paging_3.dependencyInjection
 
+import com.example.paging_3.data.remote.UnspalshApi
+import com.example.paging_3.model.UnsplashImage
+import com.example.paging_3.utils.Constants.BASE_URL
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
 
+@Module
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient{
+        return OkHttpClient.Builder()
+            .readTimeout(15,TimeUnit.SECONDS)
+            .connectTimeout(15,TimeUnit.SECONDS)
+            .build()
+    }
+
+    fun provideRetrofit(okHttpClient: OkHttpClient):Retrofit{
+        val contentType = MediaType.get("application/json")
+        val json = Json {
+            ignoreUnknownKeys = true
+        }
+
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(json.asConverterFactory(contentType))
+            .build()
+    }
+
+    fun provideUnsplashApi(retrofit: Retrofit):UnspalshApi{
+       return  retrofit.create(UnspalshApi::class.java)
+    }
 }
